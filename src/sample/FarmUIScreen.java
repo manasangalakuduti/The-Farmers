6a992f9d84ef03369006486bff88500e167ad067
valistelea <vstelea3@gatech.edu>
2020-09-22 19:52:55 -0400

    Adding Image + Button to FarmUIScreen

--- src/sample/FarmUIScreen.java
+++ src/sample/FarmUIScreen.java
@@ -2,6 +2,7 @@ package sample;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -15,12 +16,34 @@ public class FarmUIScreen extends Application {
         //Layout for Third Scene
         StackPane root = new StackPane();
         stage.setTitle("FarmUI Screen");
-        GridPane grid2 = new GridPane();
-        scene3 = new Scene(grid2, 900, 600);
-        grid2.setPadding(new Insets(10, 10, 10, 10));
-        grid2.setVgap(8);
-        grid2.setHgap(10);
-        grid2.setStyle("-fx-background-color: LemonChiffon");
+        GridPane grid3 = new GridPane();
+        scene3 = new Scene(grid3, 900, 600);
+        grid3.setPadding(new Insets(10, 10, 10, 10));
+        grid3.setVgap(8);
+        grid3.setHgap(10);
+        grid3.setStyle("-fx-background-color: LemonChiffon");
+        grid3.setStyle("-fx-background-image: url(/sample/farm.png);"
+                + "-fx-background-size: 900px 600px;"
+                + "-fx-padding-top: 100%;");
+
+
+        //Return Button
+        Button b4 = new Button("Return");
+        b4.setStyle("-fx-background-color: DeepSkyBlue; -fx-text-fill: black;"
+                + "fx-border-radius: 10; -fx-background-radius: 10;");
+        grid3.setRowIndex(b4, 0);
+        grid3.setColumnIndex(b4, 81);
+        b4.setOnAction(e -> {
+            Stage returnStage = new Stage();
+            ConfigurationScreen c = new ConfigurationScreen();
+            try {
+                c.start(returnStage);
+                stage.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+        grid3.getChildren().addAll(b4);
         stage.setScene(scene3);
         stage.show();
     }
