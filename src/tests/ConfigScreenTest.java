2d18f338f0747d955854927dbd9fcf1de18b1f4a
valistelea <vstelea3@gatech.edu>
2020-09-23 21:03:24 -0400

    Adding first JUnits Tests for ConfigScreen

--- src/tests/ConfigScreenTest.java
+++ src/tests/ConfigScreenTest.java
@@ -1,3 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+import sample.frontend.ConfigurationScreen;
+import static org.junit.Assert.assertEquals;
+
+
 public class ConfigScreenTest {
-    
+
+    private ConfigurationScreen obj;
+    @Before
+    public void setup() {
+        obj = new ConfigurationScreen();
+    }
+
+    @Test
+    public void validNameInputTest() {
+        assertEquals("", obj.validateNameInput("Vali"));
+    }
+
+    @Test
+    public void invalidNameInputTest() {
+        assertEquals("Length of name cannot be zero.", obj.validateNameInput(""));
+    }
+
 }
