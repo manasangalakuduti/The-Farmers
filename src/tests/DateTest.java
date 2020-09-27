5e3c77244eec6767e2bb129a036c2b974f2f720f
Sidharta Vadaparty <svadaparty3@gatech.edu>
2020-09-27 19:36:25 -0400

    Sid's datetest screwup

--- src/tests/DateTest.java
+++ src/tests/DateTest.java
@@ -4,7 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 import sample.backend.Date;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
@@ -14,16 +16,21 @@ import static org.junit.Assert.assertEquals;
 public class DateTest {
 
     private Date time;
+    private int curDay;
     //private LocalDateTime datetime;
 
     @Before
     public void setup() {
-        time = new Date("Fall", LocalDateTime.now());
+        LocalDateTime ref = new LocalDateTime(new LocalDate(2020, 9, 28), new LocalTime(19, 30, 0, 0));
+        int curDay = ref.getDayOfYear();
+        System.out.println("CurDay" + curDay);
+        time = new Date("Fall", ref);
     }
 
     @Test
     public void testTimestamp() {
-        assertEquals(LocalDateTime.now(), time.getDate().now());
+        int currY = time.getDate().getDayOfYear();
+        assertEquals(curDay, time.getDate().getDayOfYear());
     }
 
     @Test
