b6f5fd1ac9ac66030c745f15aec29a04e26ea15d
ksubramanian40 <ksubramanian40@gatech.edu>
2020-09-27 18:06:15 -0400

    Putting the test folder on the same level as src folder

--- tests/TestRunner.java
+++ tests/TestRunner.java
@@ -3,12 +3,13 @@ package tests;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-import java.util.List;
+
 
 public class TestRunner {
     public static void main(String[] args) {
 
-        Result result = JUnitCore.runClasses(ConfigScreenTest.class, FarmUITest.class, DateTest.class);
+        Result result = JUnitCore.runClasses(ConfigScreenTest.class,
+                FarmUITest.class, DateTest.class);
 
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
