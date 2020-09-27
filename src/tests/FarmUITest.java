db3b8420cf3d19d17af38a2d07281b173750a9e7
ksubramanian40 <ksubramanian40@gatech.edu>
2020-09-26 20:57:45 -0400

    JUnit tests for Date object and updating Player's balance

--- src/tests/FarmUITest.java
+++ src/tests/FarmUITest.java
@@ -1,6 +1,12 @@
+import sample.backend.Date;
 import sample.backend.Player;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.Assert.assertEquals;
 public class FarmUITest {
     private Player obj;
@@ -22,6 +28,22 @@ public class FarmUITest {
     @Test
     public void testPurchaseFailure() {
         assertEquals(false, obj.purchaseObject(35));
+    }
+
+    @Test
+    public void updateBalanceAfterPurchase() {
+        obj.purchaseObject(30);
+        double resBalance = obj.getBalance();
+        DecimalFormat f = new DecimalFormat("##.00");
+        assertEquals("3.01", f.format(resBalance));
+    }
+
+    @Test
+    public void testCurrentDate() {
+        Date d = new Date("Summer", LocalDateTime.now());
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        assertEquals(LocalDateTime.now().format(format),
+                d.getDate().now().format(format));
     }
 
 
