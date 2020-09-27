793e6bf9bccbc165ef14c0494480c38f99b5898c
Cole, Connor J <ccole62@gatech.edu>
2020-09-27 18:47:29 -0400

    Adding Connors JUnit tests on plots to ConnorBranch

--- src/tests/TestRunner.java
+++ src/tests/TestRunner.java
@@ -3,17 +3,13 @@ package tests;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-<<<<<<< HEAD:src/tests/TestRunner.java
-//import java.util.List;
-=======
 
->>>>>>> fc21630169924f3463fa51a8ac4370f001c023ee:tests/TestRunner.java
 
 public class TestRunner {
     public static void main(String[] args) {
 
         Result result = JUnitCore.runClasses(ConfigScreenTest.class,
-                FarmUITest.class, DateTest.class);
+                FarmUITest.class, DateTest.class, PlotTest.class);
 
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
