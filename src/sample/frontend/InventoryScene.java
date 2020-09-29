74dd3d0ed1a80c66a7b38e6cefbec4824412e8db
valistelea <vstelea3@gatech.edu>
2020-09-29 17:48:51 -0400

    Enhancing FarmUI and creating InventoryScene

--- src/sample/frontend/InventoryScene.java
+++ src/sample/frontend/InventoryScene.java
@@ -4,9 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class InventoryScene extends Application {
