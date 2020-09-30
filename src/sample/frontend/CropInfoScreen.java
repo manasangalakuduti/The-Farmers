9bf15d2a1ba1efbebd025811cd7de8b17de4c4b1
Cole, Connor J <ccole62@gatech.edu>
2020-09-29 20:53:40 -0400

    Cleaned up farm ui, added new button features

--- src/sample/frontend/CropInfoScreen.java
+++ src/sample/frontend/CropInfoScreen.java
@@ -1,32 +1,44 @@
 package sample.frontend;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Popup;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
 public class CropInfoScreen extends Application {
 
-    private Scene scene3;
     @Override
     public void start(Stage stage) throws Exception {
-        // create a label
-        Label label = new Label("This is a Popup");
-        // create a popup
-        Popup popup = new Popup();
-        // set background
-        label.setStyle(" -fx-background-color: white;");
-        // add the label
-        popup.getContent().add(label);
-        // set size of label
-        label.setMinWidth(80);
-        label.setMinHeight(50);
-        GridPane grid4 = new GridPane();
-        grid4.getChildren().addAll(label);
-        Scene scene4 = new Scene(grid4, 1920, 600);
+
+        BorderPane bPane = new BorderPane();
+        Button closeButton = new Button("Close");
+        closeButton.setStyle("-fx-background-color: DeepSkyBlue; -fx-text-fill: black;"
+                + "fx-border-radius: 10; -fx-background-radius: 10;");
+        closeButton.setOnAction(e -> {
+            try {
+                stage.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+        bPane.setLeft(closeButton);
+        bPane.setStyle("-fx-background-image: url(/sample/media/cropInfoBk.png);"
+                + "-fx-background-size: 900px 600px;"
+                + "-fx-padding-top: 100%;");
+        bPane.setPadding(new Insets(20, 20, 20, 20));
+
+        Rectangle title = new Rectangle();
+        //title.setAccessibleText(player.getName());
+        //bPane.setTop(title);
+
+
+
+        Scene scene = new Scene(bPane, Main.X_WIDTH, Main.Y_WIDTH);
+        stage.setScene(scene);
         stage.show();
 
 
