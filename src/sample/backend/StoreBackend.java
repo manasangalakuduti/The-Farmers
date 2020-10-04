901af0f51aaadd021a6b975d921b23dd79b2a032
Sidharta Vadaparty <svadaparty3@gatech.edu>
2020-10-04 15:41:35 -0400

    SidBranch StoreBackend skeletally started

---
+++ src/sample/backend/StoreBackend.java
@@ -0,0 +1,25 @@
+package sample.backend;
+
+public class StoreBackend {
+    private String Name;
+    public StoreBackend(String name) {
+        this.Name = name;
+    }
+    public double purchase(String item, int qty) {
+        //calculate money that the player would recieve and whether a store has capacity at the moment.
+        return -1;
+    }
+    public boolean sell(String item, int qty) {
+        //add a bunch of nested if statements about player's capacity, balance,
+        // and store's inventory, return true if all conditions are met
+        // return false otherwise
+        if(3 % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+    public void restock() {
+        //change out the contents of the inventory.
+        System.out.println(Name.toUpperCase() + " restocked with new items in inventory");
+    }
+}
