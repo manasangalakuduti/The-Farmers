bcb1b4277077bc66e4de547da72579f1179e49b9
Sidharta Vadaparty <svadaparty3@gatech.edu>
2020-10-10 16:02:07 -0400

    Additional StoreBackend JUnits

--- src/tests/StoreBackendTest.java
+++ src/tests/StoreBackendTest.java
@@ -16,38 +16,35 @@ public class StoreBackendTest {
     public void setup() {
         Player.initialize("Howdy", 1000);
         Player.updateInventory("Corn", 20);
-        System.out.println("OLD PLAYER CORN: " + Player.getQuantityOf("Corn"));
         obj = new StoreBackend("WalMart", new Market(new Date("Fall", LocalDateTime.now()), "Hard"));
         obj.restock();
     }
 
     @Test
     public void testSellSuccess() {
-        System.out.println("Sell success");
         obj.sell("Corn", 1);
-        System.out.println("NEW PLAYER CORN: " + Player.getQuantityOf("Corn"));
         assertEquals(21, Player.getQuantityOf("Corn"));
     }
 
     @Test
+    public void testSellFailTooMuch() {
+        obj.sell("Tomato", 30);
+        assertEquals(0, Player.getQuantityOf("Tomato"));
+    }
+
+    @Test
     public void testBuySuccess() {
-        System.out.println("Buy Success");
         obj.purchase("Corn", 15);
-        System.out.println("NEW PLAYER CORN: " + Player.getQuantityOf("Corn"));
         assertEquals(5, Player.getQuantityOf("Corn"));
     }
     @Test
     public void testBuyFailureHas() {
-        System.out.println("Buy failure 1");
         obj.purchase("Tomato", 25);
-        System.out.println("NEW PLAYER CORN: " + Player.getQuantityOf("Corn"));
         assertEquals(0, Player.getQuantityOf("Tomato"));
     }
     @Test
     public void testBuyTooMuch() {
-        System.out.println("Buy failure 2");
         obj.purchase("Corn", 30);
-        System.out.println("NEW PLAYER CORN: " + Player.getQuantityOf("Corn"));
         assertEquals(20, Player.getQuantityOf("Corn"));
     }
 }
