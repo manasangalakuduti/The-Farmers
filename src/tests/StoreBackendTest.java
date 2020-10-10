a168b7606ef713b7d5f0c8e8ec0178666f23c026
Sidharta Vadaparty <svadaparty3@gatech.edu>
2020-10-10 15:56:45 -0400

    JUnit Tests complete AND corrected StoreBackend so it updates inventory of player

--- src/tests/StoreBackendTest.java
+++ src/tests/StoreBackendTest.java
@@ -14,47 +14,40 @@ public class StoreBackendTest {
     private StoreBackend obj;
     @Before
     public void setup() {
+        Player.initialize("Howdy", 1000);
+        Player.updateInventory("Corn", 20);
+        System.out.println("OLD PLAYER CORN: " + Player.getQuantityOf("Corn"));
         obj = new StoreBackend("WalMart", new Market(new Date("Fall", LocalDateTime.now()), "Hard"));
+        obj.restock();
     }
 
     @Test
     public void testSellSuccess() {
-        Player.initialize("Bob", 33.35);
-        String item = "Tomato";
-        int qty = 3;
-        assertEquals(false, obj.sell(item, qty));
-    }
-    /*
-    @Test
-    public void testSellFailureBalance(){
-        String item = "Tomato";
-        int qty = 100;
-        assertEquals(false, obj.sell(item, qty));
-    }
-    @Test
-    public void testSellFailureRoom(){
-        String item = "Tomato";
-        int qty = 100;
-        assertEquals(false, obj.sell(item, qty));
-    }
-    @Test
-    public void testBuySuccess(){
-        Player p1 = new Player("Bob", 33.35);
-        String item = "Tomato";
-        int qty = 3;
-        assertEquals(true, obj.sell(item, qty));
+        System.out.println("Sell success");
+        obj.sell("Corn", 1);
+        System.out.println("NEW PLAYER CORN: " + Player.getQuantityOf("Corn"));
+        assertEquals(21, Player.getQuantityOf("Corn"));
     }
 
     @Test
-    public void testBuyFailureBalance(){
-        String item = "Tomato";
-        int qty = 100;
-        assertEquals(false, obj.sell(item, qty));
+    public void testBuySuccess() {
+        System.out.println("Buy Success");
+        obj.purchase("Corn", 15);
+        System.out.println("NEW PLAYER CORN: " + Player.getQuantityOf("Corn"));
+        assertEquals(5, Player.getQuantityOf("Corn"));
     }
     @Test
-    public void testBuyFailureRoom(){
-        String item = "Tomato";
-        int qty = 100;
-        obj.sell(item, qty);
-    }*/
+    public void testBuyFailureHas() {
+        System.out.println("Buy failure 1");
+        obj.purchase("Tomato", 25);
+        System.out.println("NEW PLAYER CORN: " + Player.getQuantityOf("Corn"));
+        assertEquals(0, Player.getQuantityOf("Tomato"));
+    }
+    @Test
+    public void testBuyTooMuch() {
+        System.out.println("Buy failure 2");
+        obj.purchase("Corn", 30);
+        System.out.println("NEW PLAYER CORN: " + Player.getQuantityOf("Corn"));
+        assertEquals(20, Player.getQuantityOf("Corn"));
+    }
 }
