1bd06578f3f16f3701f1bdce4caa8f94ab04a4c7
Connor <ccole62@gatech.edu>
2020-11-20 19:05:05 -0500

    Added in sound, alert for limit reached

--- src/sample/frontend/CropInfoScreen.java
+++ src/sample/frontend/CropInfoScreen.java
@@ -115,7 +115,9 @@ public class CropInfoScreen extends Application {
                 + "fx-border-radius: 20; -fx-background-radius: 10;");
         waterButton.setOnAction(e -> {
             if (!this.plot.getWateredToday()) {
-                if (Player.getCurrWater() > 0) {
+                if (Player.getCurrWater() <= 0){
+                    waterButton.setText("Water limit reached!");
+                } else if (Player.getCurrWater() > 0) {
                     this.plot.water();
                     this.plot.setWateredToday(true);
                     TransitionScene tScene = new TransitionScene();
@@ -200,7 +202,9 @@ public class CropInfoScreen extends Application {
                 + "fx-border-radius: 20; -fx-background-radius: 10;");
         harvestButton.setOnAction(e -> {
             if (this.plot.getSeedStatus().equals("Mature")) {
-                if (Player.hasRoom(1)) {
+                if (Player.getCurrHarvest() <= 0){
+                    harvestButton.setText("Harvest limit reached!");
+                } else if (Player.hasRoom(1)) {
                     if (Player.getCurrHarvest() > 0) {
                         Player.updateInventory(this.plot.getSeedType(), this.plot.getHarvestQuantity());
                         this.plot.clear();
